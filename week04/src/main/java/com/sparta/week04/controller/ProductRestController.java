29112eabd7435bdf1bd7f7421b6882e974a2be8e
Nayujin <00nyj@sookmyung.ac.kr>
2022-07-10 03:06:00 +0900

    feat: 7_관심 상품 등록하기

--- week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -2,8 +2,12 @@ package com.sparta.week04.controller;
 
 import com.sparta.week04.domain.Product;
 import com.sparta.week04.domain.ProductRepository;
+import com.sparta.week04.domain.ProductRequestDto;
+import com.sparta.week04.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,10 +15,18 @@ import java.util.List;
 @RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductRestController {
+    private final ProductService productService;
     private final ProductRepository productRepository;
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+    // 신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        productRepository.save(product);
+        return product;
     }
 }
--- week04/src/main/java/com/sparta/week04/service/ProductService.java
+++ week04/src/main/java/com/sparta/week04/service/ProductService.java
@@ -1,4 +1,22 @@
 package com.sparta.week04.service;
 
+import com.sparta.week04.domain.Product;
+import com.sparta.week04.domain.ProductMypriceRequestDto;
+import com.sparta.week04.domain.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
+@Service // 서비스임을 선언합니다.
 public class ProductService {
+    private final ProductRepository productRepository;
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언합니다.
+    public Long update(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+        product.update(requestDto);
+        return id;
+    }
 }
