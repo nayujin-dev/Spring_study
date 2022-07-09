c9f9f1d7077030b8f724d54c1c6ecbb21c0261fa
Nayujin <00nyj@sookmyung.ac.kr>
2022-07-10 02:59:43 +0900

    feat: 6_관심 상품 조회하기

--- week04/src/main/java/com/sparta/week04/Week04Application.java
+++ week04/src/main/java/com/sparta/week04/Week04Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Week04Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Week04Application.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(Week04Application.class, args);
+        }
     }
-
-}
---
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -0,0 +1,20 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.domain.Product;
+import com.sparta.week04.domain.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
+@RestController // JSON으로 데이터를 주고받음을 선언합니다.
+public class ProductRestController {
+    private final ProductRepository productRepository;
+    // 등록된 전체 상품 목록 조회
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
