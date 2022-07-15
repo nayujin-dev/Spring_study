20b08b1aafcdb1d6a74f8893956bf6fa3f8e34ca
Nayujin <00nyj@sookmyung.ac.kr>
2022-07-15 21:14:50 +0900

    feat: 9_네이버 API와 서비스 연결하기

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,13 +1,16 @@
 package com.sparta.week04.utils;
 
 import com.sparta.week04.dto.ItemDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.json.JSONObject;
 import org.json.*;
 import java.util.*;
 import java.util.List;
 
+@Component@RequiredArgsConstructor
 public class NaverShopSearch {
     public String search(String query) {
         RestTemplate rest = new RestTemplate();
@@ -15,25 +18,25 @@ public class NaverShopSearch {
         headers.add("X-Naver-Client-Id", "CJ3YtKeRuMsr2ikvZM8c");
         headers.add("X-Naver-Client-Secret", "9zFoLsGn0Z");
         String body = "";
-
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
         System.out.println("Response status: " + status);
         System.out.println(response);
-        return(response);
+        return response;
     }
+
     public List<ItemDto> fromJSONtoItems(String result) {
         JSONObject rjson = new JSONObject(result);
         JSONArray items = rjson.getJSONArray("items");
         List<ItemDto> ret = new ArrayList<>();
         for (int i=0; i<items.length(); i++) {
-            JSONObject itemJson = items.getJSONObject(i);
-            System.out.println(itemJson);
+            JSONObject itemJson = (JSONObject) items.get(i);
             ItemDto itemDto = new ItemDto(itemJson);
             ret.add(itemDto);
+
         }
 
         return ret;
