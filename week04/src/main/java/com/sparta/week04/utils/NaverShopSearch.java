4521bd33adbd2f2cb17a63f385b1c9b3be3a6a98
Nayujin <00nyj@sookmyung.ac.kr>
2022-07-15 20:56:50 +0900

    feat: 8_키워드로 상품 검색하기

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,18 +1,23 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.dto.ItemDto;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
+import org.json.*;
+import java.util.*;
+import java.util.List;
 
 public class NaverShopSearch {
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", "");
-        headers.add("X-Naver-Client-Secret", "");
+        headers.add("X-Naver-Client-Id", "CJ3YtKeRuMsr2ikvZM8c");
+        headers.add("X-Naver-Client-Secret", "9zFoLsGn0Z");
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=nctdream", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -20,9 +25,25 @@ public class NaverShopSearch {
         System.out.println(response);
         return(response);
     }
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            System.out.println(itemJson);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+
+        return ret;
+    }
+
     public static void main(String[] args) {
         NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+        String ret = naverShopSearch.search("엔시티드림");
+        naverShopSearch.fromJSONtoItems(ret);
+
     }
 
 }
